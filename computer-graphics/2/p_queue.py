6890ca90b9983557fe64a9101fb9b616eb63d691
Eugen Vorobiov <jeka.vorobjov@gmail.com>
2022-06-06 22:56:39 +0300

    labs finished

---
+++ computer-graphics/2/p_queue.py
@@ -0,0 +1,43 @@
+import heapq
+import itertools
+
+
+class PriorityQueue:
+    def __init__(self):
+        self.pq = []
+        self.entry_finder = {}
+        self.counter = itertools.count()
+
+    def push(self, item):
+        # check for duplicate
+        if item in self.entry_finder:
+            return
+        count = next(self.counter)
+        # use x-coordinate as a primary key (heapq in python is min-heap)
+        entry = [item, count, item]
+        self.entry_finder[item] = entry
+        heapq.heappush(self.pq, entry)
+
+    def remove_entry(self, item):
+        entry = self.entry_finder.pop(item)
+        entry[-1] = 'Removed'
+
+    def pop(self):
+        while self.pq:
+            priority, count, item = heapq.heappop(self.pq)
+            if item is not 'Removed':
+                del self.entry_finder[item]
+                return item
+
+
+    def top(self):
+        while self.pq:
+            priority, count, item = heapq.heappop(self.pq)
+            if item is not 'Removed':
+                del self.entry_finder[item]
+                self.push(item)
+                return item
+        raise KeyError('top from an empty priority queue')
+
+    def empty(self):
+        return not self.pq
